669aee151b785559183366dbd4a890ca41cd3e84
cfiander <cfiander90@gmail.com>
2019-07-22 12:21:38 -0700

    five done

--- ReverseString.js
+++ ReverseString.js
@@ -1,3 +1,7 @@
-function reverseString(string) {
-    
-}
+function reverseString(str) {
+    if (str === "")
+    return "";
+    else
+    return reverseString(str.substr(1)) + str.charAt(0);
+  }
+  console.log(reverseString("hello"))
---
+++ TriangularNumber.js
@@ -0,0 +1,8 @@
+function nth(num) {
+    if (num <= 1 ) {
+      return 1
+    }
+    return num + nth(num - 1)
+  }
+  
+  console.log(nth(9))
